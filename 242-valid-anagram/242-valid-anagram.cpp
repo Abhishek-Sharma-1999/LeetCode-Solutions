964d1fee292e3d42ac9b0d0a14e9ee79327c086d
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-08-06 22:36:57 +0530

    Time: 21 ms (29.10%), Space: 7.2 MB (94.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -3,8 +3,20 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())
             return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s==t;
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        for(auto i:mp)
+        {
+            if(i.second!=0)
+                return false;
+        }
+        return true;
     }
 };
