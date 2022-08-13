02e630e5953c1c6a3a3926cf0374ca2465bb1d72
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-08-14 00:11:31 +0530

    Time: 0 ms (100.00%), Space: 6 MB (16.80%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 || n==1)
+        // Recursive Way
+        // if(n==0 || n==1)
+        //     return n;
+        // return fib(n-1)+fib(n-2);
+        
+        //DP
+        if(n<2)
             return n;
-        return fib(n-1)+fib(n-2);
+        int ans[n+1];
+        ans[0]=0;
+        ans[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            ans[i]=ans[i-1]+ans[i-2];
+        }
+        return ans[n];
     }
 };
