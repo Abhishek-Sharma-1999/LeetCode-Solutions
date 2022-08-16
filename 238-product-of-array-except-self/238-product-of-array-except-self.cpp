0b1876d8ef1bd4040266482d2f8d7e5285299849
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-08-16 13:55:47 +0530

    Time: 39 ms (53.37%), Space: 25.1 MB (18.29%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
+        int n=nums.size();
+        vector<int> left(n);
+        vector<int> right(n);
         vector<int> ans(n);
-        vector<int> left_Product(n);
-        vector<int> right_Product(n);
         
-        left_Product[0] = 1;
-        for(int i = 1; i <n; i++){
-            left_Product[i] = left_Product[i - 1] * nums[i - 1];
+        left[0]=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            left[i]=left[i-1]*nums[i-1];
         }
-        
-        right_Product[n - 1] = 1;
-        for (int j = n - 2; j >= 0; j--) {
-            right_Product[j] = right_Product[j + 1] * nums[j + 1];
+        right[n-1]=1;
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=right[i+1]*nums[i+1];
         }
-        
-        for (int k = 0; k < n; k++) {
-            ans[k]= left_Product[k] * right_Product[k];
+        for(int i=0;i<n;i++)
+        {
+            ans[i]=left[i]*right[i];
         }
         
         return ans;
