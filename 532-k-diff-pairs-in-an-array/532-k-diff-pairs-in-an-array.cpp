9155d8d5c80a163d969e357d79a33c8134376264
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-08-23 00:35:45 +0530

    Time: 15 ms (94.31%), Space: 11.2 MB (83.43%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,24 +1,54 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-     unordered_map<int,int> mp;
-        for(auto i:nums)
-        {
-            mp[i]++;
-        }
+// Map Solution T.C: O(n)  S.C.: O(n)
+//         unordered_map<int,int> mp;
+//         for(auto i:nums)
+//         {
+//             mp[i]++;
+//         }
+//         int ans=0;
+        
+//         for(auto x:mp)
+//         {
+//             if(k==0)
+//             {
+//                 if(x.second>1)
+//                     ans++;
+//             }
+//             else if(mp.find(x.first+k)!=mp.end())
+//                 ans++;
+//         }
+        
+//         return ans;
+        
+        sort(nums.begin(),nums.end());
+        int slow=0,fast=1;
         int ans=0;
-        
-        for(auto x:mp)
+        while(slow<nums.size() && fast<nums.size())
         {
-            if(k==0)
+            if(nums[fast]-nums[slow]==k)
             {
-                if(x.second>1)
-                    ans++;
-            }
-            else if(mp.find(x.first+k)!=mp.end())
                 ans++;
+                slow++;
+                fast++;
+                while(fast<nums.size() && nums[fast]==nums[fast-1])
+                {
+                    fast++;
+                }
+                
+            }
+            
+            else if(nums[fast]-nums[slow] > k)
+            {
+                slow++;
+                if(fast-slow==0) // agar dono same position par aa gate, toh fast ko aage incerement kar denge
+                    fast++;
+            }
+            
+            else
+                fast++;          
         }
-        
         return ans;
     }
 };
