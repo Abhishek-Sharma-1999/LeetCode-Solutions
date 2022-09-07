dd2c678d6ec2aa4158029afff32f9c48c257477c
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-09-07 17:27:32 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (67.14%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int ans=0;
+        for(int i=0;i<s.length();i++)
+        {
+            ans=ans^s[i];
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            ans=ans^t[i];
+        }
+        
+        return ans;
+    }
+};
