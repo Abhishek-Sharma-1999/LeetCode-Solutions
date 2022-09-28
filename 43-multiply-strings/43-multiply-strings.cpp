2ce1ea3d7a8458e6ee03b91429fb7b5a160e70db
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-09-28 21:05:38 +0530

    Time: 14 ms (47.83%), Space: 6.6 MB (70.29%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+     if(num1=="0" || num2=="0")
+     {
+         return "0";
+     }
+        vector<int> res(num1.size()+num2.size(),0);
+        for(int i=num1.size()-1;i>=0;i--)
+        {
+            for(int j=num2.size()-1;j>=0;j--)
+            {
+                res[i+j+1]+=(num1[i]-'0')*(num2[j]-'0');
+                res[i+j]+=res[i+j+1]/10;
+                res[i+j+1]=res[i+j+1]%10;
+            }
+        }
+        int i=0;
+        string ans="";
+        // traversing starting zeroes if present as we have created vector and initializing it with zeo
+        while(res[i]==0)
+        {
+            i++;
+        }
+        while(i<res.size())
+        {
+            ans.push_back(res[i]+'0');  // Converting number back to string
+            i++;
+        }
+        return ans;
+    }
+};
