8f47f94fd1459b78705100fe637db81d13823c0b
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-09-30 23:57:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (66.89%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n=word.size();
+        int Upper=0;
+        for(int i=0;i<n;i++)
+        {
+            if(word[i]>='A' && word[i]<='Z')
+            {
+                Upper++;
+            }
+        }
+        
+        if(Upper==n || Upper==0 || (word[0]>='A' && word[0]<='Z' && Upper==1))
+        {
+            return 1;
+        }
+        
+        return 0;
+    }
+};
