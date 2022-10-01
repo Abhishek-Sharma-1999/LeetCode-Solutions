e022d4283a47e49fb391c97b9f43023337d49435
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-10-01 14:51:53 +0530

    Time: 28 ms (65.70%), Space: 11.2 MB (85.16%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,26 +1,58 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        for(auto i:nums)
-        {
-            mp[i]++;
-        }
+        // Approach-1
+        // T.C- O(n)        S.C.- O(n)
+        // unordered_map<int,int> mp;
+        // for(auto i:nums)
+        // {
+        //     mp[i]++;
+        // }
+        // int ans=0;
+        // for(auto i:mp)
+        // {
+        //     if(k==0)
+        //     {
+        //         if(i.second>1)
+        //         {
+        //             ans++;
+        //         }
+        //     }
+        //     else if(mp.find(i.first+k)!=mp.end())
+        //     {
+        //         ans++;
+        //     }
+        // }
+        // return ans;
+        // Approach -2
+        // T.C.-O(n)        S.C.-O(n)
+       sort(nums.begin(),nums.end());
+        int slow=0,fast=1;
         int ans=0;
-        for(auto i:mp)
+        while(slow<nums.size() && fast<nums.size())
         {
-            if(k==0)
-            {
-                if(i.second>1)
-                {
-                    ans++;
-                }
-            }
-            else if(mp.find(i.first+k)!=mp.end())
+            if(nums[fast]-nums[slow]==k)
             {
                 ans++;
+                slow++;
+                fast++;
+                while(fast<nums.size() && nums[fast]==nums[fast-1])
+                {
+                    fast++;
+                }
+                
             }
+            
+            else if(nums[fast]-nums[slow] > k)
+            {
+                slow++;
+                if(fast-slow==0) // agar dono same position par aa gate, toh fast ko aage incerement kar denge
+                    fast++;
+            }
+            
+            else
+                fast++;          
         }
-        return ans;
+        return ans;;
     }
 };
