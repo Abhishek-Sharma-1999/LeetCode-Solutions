b59f16be2a3cff5ce87af3593110ccbfcb6e2e15
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-10-01 14:27:22 +0530

    Time: 44 ms (29.55%), Space: 13.3 MB (57.96%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,53 +1,25 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-// Map Solution T.C: O(n)  S.C.: O(n)
-//         unordered_map<int,int> mp;
-//         for(auto i:nums)
-//         {
-//             mp[i]++;
-//         }
-//         int ans=0;
-        
-//         for(auto x:mp)
-//         {
-//             if(k==0)
-//             {
-//                 if(x.second>1)
-//                     ans++;
-//             }
-//             else if(mp.find(x.first+k)!=mp.end())
-//                 ans++;
-//         }
-        
-//         return ans;
-        
-        sort(nums.begin(),nums.end());
-        int slow=0,fast=1;
-        int ans=0;
-        while(slow<nums.size() && fast<nums.size())
+        unordered_map<int,int> mp;
+        for(auto i:nums)
         {
-            if(nums[fast]-nums[slow]==k)
+            mp[i]++;
+        }
+        int ans=0;
+        for(auto i:mp)
+        {
+            if(k==0)
+            {
+                if(i.second>1)
+                {
+                    ans++;
+                }
+            }
+            else if(mp.find(i.first+k)!=mp.end())
             {
                 ans++;
-                slow++;
-                fast++;
-                while(fast<nums.size() && nums[fast]==nums[fast-1])
-                {
-                    fast++;
-                }
-                
             }
-            
-            else if(nums[fast]-nums[slow] > k)
-            {
-                slow++;
-                if(fast-slow==0) // agar dono same position par aa gate, toh fast ko aage incerement kar denge
-                    fast++;
-            }
-            
-            else
-                fast++;          
         }
         return ans;
     }
