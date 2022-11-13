8fdc400d6516fcefcd5327f610f42151a4465d76
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-11-13 19:18:03 +0530

    Time: 37 ms (8.61%), Space: 20.9 MB (18.80%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,31 +1,55 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        if(s.length()==0)
-            return s;
-        stack<string> st;
-        for(int i=0;i<s.length();i++)
+        // T.C.-> O(n)       S.C. -> O(n)
+        // if(s.length()==0)
+        //     return s;
+        // stack<string> st;
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(s[i]==' ')   //agar space mila toh next iteration mein aa jao
+        //         continue;
+        //     string temp;
+        //     while(i<s.length() && s[i]!=' ')    // agar space nahin mila toh use temporary string mein store karva lenge
+        //     {
+        //         temp=temp+s[i];
+        //         i++;
+        //     }
+        //     st.push(temp);
+        // }
+        // string ans;
+        // while(!st.empty())
+        // {
+        //     ans=ans+st.top();
+        //     st.pop();
+        //     if(!st.empty()) // agar stack empty nahin hai,matlab dusra word aaya hai, ek string pop out karne ke baad toh space(' ') ko add kar denge
+        //     {
+        //         ans+=' ';
+        //     }
+        // }
+        // return ans;
+        
+        
+        
+        //T.C.- O(n)        S.C.- O(1)
+        string ans;
+        int i=0;
+        while(i<s.length())
         {
-            if(s[i]==' ')   //agar space mila toh next iteration mein aa jao
-                continue;
+            while(s[i]==' ')    // for spaces
+                i++;
+            
             string temp;
-            while(i<s.length() && s[i]!=' ')    // agar space nahin mila toh use temporary string mein store karva lenge
+            
+            while(i<s.length() && s[i]!=' ')    //agar hame space nahin mila toh temporary string mein us string ko add kar denge
             {
-                temp=temp+s[i];
+                temp+=s[i];
                 i++;
             }
-            st.push(temp);
+            if(temp.empty()==false)
+                ans=temp+" "+ans;
         }
-        string ans;
-        while(!st.empty())
-        {
-            ans=ans+st.top();
-            st.pop();
-            if(!st.empty()) // agar stack empty nahin hai,matlab dusra word aaya hai, ek string pop out karne ke baad toh space(' ') ko add kar denge
-            {
-                ans+=' ';
-            }
-        }
+        ans.pop_back();
         return ans;
     }
 };
