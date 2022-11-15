ff5635722207673405dcc9c95a4b30b05f454d77
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-11-15 23:04:49 +0530

    Time: 59 ms (65.98%), Space: 30.9 MB (20.93%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -12,17 +12,46 @@
 class Solution {
 public:
     // Using Any Traversal as it will also count the node
-    int count=0;
-    void inorder(TreeNode* root)
-    {
-        if(root==NULL)
-            return;
-        inorder(root->left);
-        count++;
-        inorder(root->right);
-    }
+    // int count=0;
+    // void inorder(TreeNode* root)
+    // {
+    //     if(root==NULL)
+    //         return;
+    //     inorder(root->left);
+    //     count++;
+    //     inorder(root->right);
+    // }
     int countNodes(TreeNode* root) {
-        inorder(root);
-        return count;
+        // inorder(root);
+        // return count;
+    
+    // 2-Approach
+    if(root==NULL)
+        return 0;
+     int Left_Height=0,Right_Height=0;
+    TreeNode *curr=root;
+    // Calculating Left Height
+    while(curr!=NULL)
+    {
+        Left_Height++;
+        curr=curr->left;
+    }
+        curr=root;
+    // Calculating Right Height
+    while(curr!=NULL)
+    {
+        Right_Height++;
+        curr=curr->right;
+    }
+    
+    if(Left_Height==Right_Height)
+    {
+        return pow(2,Left_Height)-1;    //2^h -1, where h is height of tree
+    }
+    
+        else
+            return 1+countNodes(root->left)+countNodes(root->right);
+        // T.C.- O(Log N)^2     First Log-for calculating heights 
+       //                       Second Log-for calculating Nodes
     }
 };
