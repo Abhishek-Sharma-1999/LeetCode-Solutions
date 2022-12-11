c6f58aa5439860344b6c9af4e0d0f0e1e994fe55
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2022-12-11 17:27:18 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (82.72%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.cpp
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        stack<int>st;
-        int score=0;
-        for(int i=0;i<s.length();i++)
+       // Stack Solution   T.C.-> O(n)      S.C.-> O(n)
+        // stack<int>st;
+        // int score=0;
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(s[i]=='(')
+        //     {
+        //         st.push(score);
+        //         score=0;
+        //     }
+        //     else
+        //     {
+        //         score=st.top()+max(1,2*score);
+        //         st.pop();
+        //     }
+        // }
+        // return score;
+        
+        // Optimised Solution  T.C.-> O(n)      S.C.-> O(1)
+        int depth=0,res=0;
+        char prev='(';
+        for(auto i:s)
         {
-            if(s[i]=='(')
+            if(i=='(')
             {
-                st.push(score);
-                score=0;
+                depth++;
             }
             else
             {
-                score=st.top()+max(1,2*score);
-                st.pop();
+                depth--;
+                if(prev=='(')
+                    res=res+pow(2,depth);
             }
+            prev=i;
         }
-        return score;
+        return res;
     }
 };
