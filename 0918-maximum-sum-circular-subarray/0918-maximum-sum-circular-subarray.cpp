b630d7f1c595f8255d99481d3e3fcaa9c5c72619
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2023-01-18 23:40:45 +0530

    Time: 63 ms (91.29%), Space: 39.9 MB (38.25%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,42 +1,38 @@
 class Solution {
-public:
-    int KdanesAlgo(vector<int>& nums)
+    private:
+    int kdaneMin(vector<int>& nums,int n)
     {
-        int sum=0;
-        int maxi=INT_MIN;
-        for(int i=0;i<nums.size();i++)
-        {
-            sum=sum+nums[i];
-            maxi=max(maxi,sum);
-            if(sum<0)
-            {
-                sum=0;
-            }
+        int sum=nums[0];
+        int minSum=nums[0];
+        for(int i=1;i<n;i++){
+            sum=min(sum+nums[i],nums[i]);
+            minSum=min(minSum,sum);
         }
-        return maxi;
+        return minSum;
     }
+    
+    
+    int kdaneMax(vector<int>& nums,int n)
+    {
+        int sum=nums[0];
+        int maxSum=nums[0];
+        for(int i=1;i<n;i++){
+            sum=max(sum+nums[i],nums[i]);
+            maxSum=max(maxSum,sum);
+        }
+        return maxSum;
+    }
+public:
     int maxSubarraySumCircular(vector<int>& nums) {
         int n=nums.size();
-        if(n==1)
+        int SUM=accumulate(begin(nums),end(nums),0);
+        int minSum=kdaneMin(nums,n);
+        int maxSum=kdaneMax(nums,n);
+        int CircularSum=SUM-minSum;
+        if(maxSum>0)
         {
-            return nums[0];
+            return max(maxSum,CircularSum);
         }
-        int linear_Sum=KdanesAlgo(nums);
-        
-        if(linear_Sum<0)    // If all elements are Negative
-        {
-            return linear_Sum;
-        }
-        
-        int sum=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            sum=sum+nums[i];
-            nums[i]=-nums[i];
-        }
-        
-        int Circular_Sum=sum+KdanesAlgo(nums);
-        
-        return max(linear_Sum,Circular_Sum);
+        return maxSum;
     }
 };
