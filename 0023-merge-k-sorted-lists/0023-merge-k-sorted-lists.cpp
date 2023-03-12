b976a35fdba08b421ccff341bff8f1c241cbc79e
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2023-03-12 13:42:06 +0530

    Time: 21 ms (86.20%), Space: 13.5 MB (39.63%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,27 +10,68 @@
  */
 class Solution {
 public:
+    
+    ListNode* mergeTwoSortedLists(ListNode *l1,ListNode *l2)
+    {
+        if(l1==NULL)
+            return l2;
+        
+        if(l2==NULL)
+            return l1;
+        
+        if(l1->val<=l2->val)
+        {
+            l1->next=mergeTwoSortedLists(l1->next,l2);
+            return l1;
+        }
+        else
+        {
+            l2->next=mergeTwoSortedLists(l2->next,l1);
+            return l2;
+        }
+        return NULL;
+    }
+    
+    ListNode *partitionAndMerge(int start,int end,vector<ListNode*>& lists)
+    {
+        if(start>end)
+            return NULL;
+        
+        if(start==end)  //agar ek hi list hai
+            return lists[start];
+        
+        int mid=start+(end-start)/2;
+        ListNode *List1=partitionAndMerge(start,mid,lists);
+        ListNode *List2=partitionAndMerge(mid+1,end,lists);
+        return mergeTwoSortedLists(List1,List2);
+    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
        // Approach-1
-        vector<int> ans;
-        for(int i=0;i<lists.size();i++)
-        {
-            while(lists[i]!=NULL)
-            {
-                ans.push_back(lists[i]->val);
-                lists[i]=lists[i]->next;
-            }
-        }
-        sort(ans.begin(),ans.end());
-        ListNode *dummy=new ListNode();
-        ListNode *start=dummy;
-        for(int i=0;i<ans.size();i++)
-        {
-            ListNode *temp=new ListNode(ans[i]);
-            dummy->next=temp;
-            dummy=dummy->next;
-        }
-        return start->next;
+        // vector<int> ans;
+        // for(int i=0;i<lists.size();i++)
+        // {
+        //     while(lists[i]!=NULL)
+        //     {
+        //         ans.push_back(lists[i]->val);
+        //         lists[i]=lists[i]->next;
+        //     }
+        // }
+        // sort(ans.begin(),ans.end());
+        // ListNode *dummy=new ListNode();
+        // ListNode *start=dummy;
+        // for(int i=0;i<ans.size();i++)
+        // {
+        //     ListNode *temp=new ListNode(ans[i]);
+        //     dummy->next=temp;
+        //     dummy=dummy->next;
+        // }
+        // return start->next;
+        
+        //Approach-2
+        int k=lists.size();
+        if(k==0)
+            return NULL;
+        return partitionAndMerge(0,k-1,lists);
         
     }
 };
