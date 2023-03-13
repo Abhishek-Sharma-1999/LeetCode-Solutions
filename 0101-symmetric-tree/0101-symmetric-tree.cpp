2dc16e278b7542cdd27926415e28403590cce68a
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2023-03-13 22:04:01 +0530

    Time: 3 ms (88.55%), Space: 16.5 MB (58.96%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -13,19 +13,28 @@ class Solution {
 public:
     bool Match(TreeNode *root1,TreeNode *root2)
     {
-        if(root1!=NULL && root2!=NULL)
-        {
-            bool a=Match(root1->left,root2->right);
-            bool b=Match(root1->right,root2->left);
-            if(root1->val==root2->val && a && b)
-                return true;
-            else
-                return false;
-        }
-        else if(root1==NULL && root2==NULL)
+        // if(root1!=NULL && root2!=NULL)
+        // {
+        //     bool a=Match(root1->left,root2->right);
+        //     bool b=Match(root1->right,root2->left);
+        //     if(root1->val==root2->val && a && b)
+        //         return true;
+        //     else
+        //         return false;
+        // }
+        // else if(root1==NULL && root2==NULL)
+        //     return true;
+        // else
+        //     return false;
+        if(root1==NULL && root2==NULL)
             return true;
-        else
+        // dono NUll ho ye condition upar hi hmne check karli,matlab ab ek null nahin hai
+        if(root1==NULL || root2==NULL)
             return false;
+        if((root1->val==root2->val) && Match(root1->left,root2->right) && Match(root1->right,root2->left))
+            return true;
+        // agar iske alava kuch bhi hai,toh return false
+        return false;
     }
     bool isSymmetric(TreeNode* root) {
         if(root==NULL)
