02d0234a96432bb6a915bff040315ef26b8dd613
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2023-04-29 19:24:03 +0530

    Time: 25 ms (43.83%), Space: 13.9 MB (61.00%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,57 +1,51 @@
 class Solution {
-public:
-    vector<int> topologicalSort(unordered_map<int,vector<int>> &adj,int &numCourses,vector<int> &indegree)
+    vector<int> topologicalSort(unordered_map<int,vector<int>> &mp,int numCourses,vector<int> &indegree)
     {
-        queue<int> q;
-        vector<int> result;
-        int count=0;    //for checking we have completed all the courses or not
+        queue<int>q;
+        vector<int>res;
+        int count=0;
+        // jinki indegree 0 hai, unhe queue mein daal denge
         for(int i=0;i<numCourses;i++)
         {
             if(indegree[i]==0)
             {
-                result.push_back(i);
                 q.push(i);
                 count++;
+                res.push_back(i);
             }
         }
-        
         while(!q.empty())
         {
             int u=q.front();
             q.pop();
-            
-            for(int v:adj[u])
+            for(int &v:mp[u])
             {
                 indegree[v]--;
-                
                 if(indegree[v]==0)
                 {
-                    result.push_back(v);
-                    q.push(v);
                     count++;
+                    q.push(v);
+                    res.push_back(v);
                 }
+                    
             }
         }
-        
-        if(count==numCourses)
-            return result;
-        
-        return {};  //we had a cycle and we couldn't complete all the courses
+        if(numCourses==count)
+            return res;
+        return {};
     }
-    
-    
+public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>> adj;
+        unordered_map<int,vector<int>>mp;
         vector<int>indegree(numCourses,0);
-        
+        // Making ajjacency list and indegree
         for(auto &vec:prerequisites)
         {
             int a=vec[0];
             int b=vec[1];
-            adj[b].push_back(a);
+            mp[b].push_back(a);
             indegree[a]++;
-        }                                                   
-        
-        return topologicalSort(adj,numCourses,indegree);
+        }
+        return topologicalSort(mp,numCourses,indegree);
     }
 };
