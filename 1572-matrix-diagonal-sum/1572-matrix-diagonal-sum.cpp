808aa3f94a59780f3519871beaf81fbbce74291b
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2023-05-08 23:03:01 +0530

    Time: 17 ms (48.72%), Space: 11.1 MB (92.07%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int row=mat.size();
-        int col=mat[0].size();
+        // Approach -1
+        // int row=mat.size();
+        // int col=mat[0].size();
+        // int sum=0;
+        // for(int i=0;i<row;i++)
+        // {
+        //     for(int j=0;j<col;j++)
+        //     {
+        //         if(i==j)
+        //         {   //primary diagonal
+        //             sum+=mat[i][i];
+        //         }
+        //         else if(i+j==row-1)
+        //         {
+        //             sum+=mat[i][j];
+        //         }
+        //     }
+        // }
+        // return sum;
+        
+        // Approach-2 O(n)
+        int m=mat.size();
         int sum=0;
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                if(i==j)
-                {   //primary diagonal
-                    sum+=mat[i][i];
-                }
-                else if(i+j==row-1)
-                {
-                    sum+=mat[i][j];
-                }
-            }
+        for(int i=0;i<m;i++){
+            sum+=mat[i][i];
+            sum+=mat[i][m-i-1];
         }
+        if(m%2!=0)  //odd
+            sum-=mat[m/2][m/2];
         return sum;
     }
 };
