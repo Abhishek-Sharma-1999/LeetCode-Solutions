a1ee22b37b4f4f2ceb3695c040cb7c58374f69e7
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2023-05-17 22:50:52 +0530

    Time: 291 ms (16.85%), Space: 116.5 MB (99.29%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,18 +11,46 @@
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        vector<int>ans;
-        while(head!=NULL){
-            ans.push_back(head->val);
-            head=head->next;
+        // vector<int>ans;
+        // while(head!=NULL){
+        //     ans.push_back(head->val);
+        //     head=head->next;
+        // }
+        // int i=0,j=ans.size()-1,res=INT_MIN;
+        // while(i<j)
+        // {
+        //     int sum=ans[i]+ans[j];
+        //     res=max(sum,res);
+        //     i++;
+        //     j--;
+        // }
+        // return res;
+        
+        // Approach-2 //half part ko baad mein hai,use reverse kar denge, phir 2 pointer chlayenge ek head se aur ek reverse vaale part se aur sum lete hue chalenge,aur jaise hi reverse part vaala pointer null pe hoga, ruk jaayenge
+        ListNode *slow=head;
+        ListNode *fast=head;
+       // calculating middle of linked list 
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        int i=0,j=ans.size()-1,res=INT_MIN;
-        while(i<j)
-        {
-            int sum=ans[i]+ans[j];
-            res=max(sum,res);
-            i++;
-            j--;
+        // reversing next half part of linked list
+        ListNode *mid=slow;
+        ListNode *prev=NULL,*forward=NULL;
+        while(mid!=NULL){
+            forward=mid->next;
+            mid->next=prev;
+            prev=mid;
+            mid=forward;
+        }
+        // now calculating the result
+        int res=0;
+        ListNode*first=head;    //pointer of first node
+        // pointer of head of second list will be prev
+        while(prev!=NULL){
+            res=max(res,first->val+prev->val);
+            first=first->next;
+            prev=prev->next;
         }
         return res;
     }
