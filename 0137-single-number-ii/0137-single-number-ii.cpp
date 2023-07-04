45349ce6c5bc83dfe95e3ac549fee8e72b6c6ae3
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2023-07-04 22:35:19 +0530

    Time: 6 ms (84.62%), Space: 9.4 MB (98.52%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,16 +2,35 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         // Approach-1
-        unordered_map<int,int>mp;
-        for(auto i:nums)
+        // unordered_map<int,int>mp;
+        // for(auto i:nums)
+        // {
+        //     mp[i]++;
+        // }
+        // int res;
+        // for(auto i:mp)
+        // {
+        //     if(i.second==1)
+        //         res=i.first;
+        // }
+        // return res;
+        
+        // Approach-2 (Bit Manipulation)
+        int res=0;
+        for(int i=0;i<32;i++)
         {
-            mp[i]++;
-        }
-        int res;
-        for(auto i:mp)
-        {
-            if(i.second==1)
-                res=i.first;
+            int temp=(1<<i);
+            int count_one=0;
+            int count_zero=0;
+            for(int &num:nums)
+            {
+                if((num & temp)==0)
+                    count_zero++;
+                else
+                    count_one++;
+            }
+            if(count_one%3==1)
+                res=(res|temp);
         }
         return res;
     }
