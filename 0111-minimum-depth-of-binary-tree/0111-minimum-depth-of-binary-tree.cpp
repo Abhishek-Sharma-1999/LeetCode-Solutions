42e88f234c72737b986970e44a4ca884b261f40d
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2023-07-10 22:24:53 +0530

    Time: 316 ms (13.95%), Space: 144.7 MB (31.37%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,16 +12,38 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
+        // DFS ---------------------------------------
         if(root==NULL)
             return 0;
-        if(root->left==NULL)
-        {
+        if(root->left==NULL)    //agar root ka left,NULL hai toh right chale gaye
             return 1+minDepth(root->right);
-        }
-        if(root->right==NULL)
-        {
+        if(root->right==NULL) //agar root ka right NULL hai toh left chale gaye
             return 1+minDepth(root->left);
+        return 1+min(minDepth(root->left),minDepth(root->right));   //hame minimum chaiye,jo left ya right se aayega (+1) jis root par hum currently hain
+    
+        
+        // ----------------BFS------------------
+        if(root==NULL)
+            return 0;
+        queue<TreeNode*>q;
+        q.push(root);
+        int level=1;
+        while(!q.empty())
+        {
+            TreeNode* u=q.front();
+            q.pop();
+            int n=q.size();
+            while(n--)
+            {
+                if(u->left==NULL && u->right==NULL)
+                    return level;
+                if(u->left!=NULL)
+                    q.push(root->left);
+                if(u->right!=NULL)
+                    q.push(root->right);
+            }
+            level++;
         }
-        return 1+min(minDepth(root->left),minDepth(root->right));
+        return -1;  //answer upar hi return kar diya hoga hamne
     }
 };
