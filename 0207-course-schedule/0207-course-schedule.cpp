d85b6d6f8479f0f7f3b87305dbd0d4438b55ebdb
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2023-07-13 23:17:50 +0530

    Time: 24 ms (64.89%), Space: 13.9 MB (52.34%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,46 +1,41 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>> mp;
+        unordered_map<int,vector<int>>adj;  //Adjacency List
         vector<int>indegree(numCourses,0);
         for(auto &vec:prerequisites)
         {
             int a=vec[0];
             int b=vec[1];
-            mp[b].push_back(a);
-            indegree[a]++;
+            // b->a (pehle 'b' phir 'a' , b se a ki taraf arrow ja rha hai)
+            adj[b].push_back(a);
+            indegree[a]++;  //arrow a ki taraf,jaa rha hai toh 'a' ki indegree increase hogi
         }
-        vector<int>res;
-        int count=0;
-        queue<int>q;
+        queue<int>q; 
+        int count=0;    //track rakhne ke liye,ki kitne courses visit kiye hain
         for(int i=0;i<numCourses;i++)
         {
-            if(indegree[i]==0)
-            {
+            if(indegree[i]==0){  // jis ki indegree 0 hogi,use queue mein daal denge
                 q.push(i);
                 count++;
-                res.push_back(i);
             }
         }
-        // BFS
+        // Normal BFS
         while(!q.empty())
         {
             int u=q.front();
             q.pop();
-            for(int &v:mp[u])
+            for(int &v:adj[u])
             {
                 indegree[v]--;
-                if(indegree[v]==0)
-                {
+                if(indegree[v]==0){
                     q.push(v);
                     count++;
-                    res.push_back(v);
                 }
             }
         }
-        
-        if(count==numCourses)
-            return 1;
-        return 0;
+        if(count==numCourses)   //saare course visite kar liye,toh true return kar denge
+            return true;    //nahin toh false return kar denge
+        return false;
     }
 };
