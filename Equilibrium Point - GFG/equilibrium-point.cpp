eab354101ba244fac2a0f793a606c3b59da86efc
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2023-09-23 18:28:35 +0530

    Added solution - LeetHub

---
+++ Equilibrium Point - GFG/equilibrium-point.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        long long sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+a[i];
+        }
+        long long rightSum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum-a[i];
+            if(rightSum==sum){
+                return i+1;
+            }
+            rightSum+=a[i];
+        }
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
