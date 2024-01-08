bb661e11e99fb05fbdb463780285d76102330af0
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2024-01-08 22:52:18 +0530

    Time: 87 ms (89.89%), Space: 64.9 MB (88.84%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence149-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div938-range-sum-of-bst/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence149-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div938-range-sum-of-bst.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum=0;
+    int inorder(TreeNode* root, int low, int high)
+    {
+        if(root==NULL)
+            return 0;
+        inorder(root->left,low,high);
+        if(root->val>=low && root->val<=high)
+            sum+=root->val;
+        inorder(root->right,low,high);
+        return sum;
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+       //Approach-1 (In-Order Traversal)
+        inorder(root,low,high);
+        return sum;
+        // if(root==NULL)
+        // {
+        //     return 0;
+        // }
+        // int ans=0;
+        // if(root->val>=low && root->val<=high)
+        // {
+        //     ans+=root->val+rangeSumBST(root->left,low,high)+rangeSumBST(root->right,low,high);
+        //     return ans;
+        // }
+        // if(root->val>high)
+        // {
+        //     return rangeSumBST(root->left,low,high);
+        // }
+        // else
+        // {
+        //     return rangeSumBST(root->right,low,high);
+        // }
+        // return ans;
+    }
+};
