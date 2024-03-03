04ee7d0e05037c09145568b2af5b9d3c39480659
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2024-03-03 21:24:42 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (14.37%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(n--)
+        {
+            fast=fast->next;
+        }
+        // if it is first node
+        if(fast==NULL)
+        {
+            ListNode *temp=head->next;
+            delete(head);
+            return temp;
+
+        }
+        while(fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next;
+        }
+        ListNode *temp=slow->next;
+        slow->next=temp->next;
+        delete(temp);
+        return head;
+
+
+    }
+};
