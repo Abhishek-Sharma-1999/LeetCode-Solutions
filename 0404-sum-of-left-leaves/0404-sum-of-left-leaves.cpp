5d15e751a5cf21f844d3d7bb02eb93bccbf223a4
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2024-04-14 18:30:46 +0530

    Time: 8 ms (8.78%), Space: 14.8 MB (15.62%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int solve(TreeNode* root,bool isLeft){
+        if(root==NULL){
+            return 0;
+        }
+        if(root->left==NULL && root->right==NULL && isLeft==true){
+            return root->val;
+        }
+        return solve(root->left,true)+solve(root->right,false);
+    }
+    int sumOfLeftLeaves(TreeNode* root) {
+        return solve(root,false);
+    }
+};
